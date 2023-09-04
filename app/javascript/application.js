154befc350af5b628735eefd3560a1b5d94068a8
Katie Bridson <kbridson@memphis.edu>
2023-09-04 06:39:11 -0500

    Update to Rails 7.0.7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,13 +4,15 @@ import "controllers"
 
 import "popper"
 import "bootstrap"
-import autosize from "autosize"
+import * as autosize from "autosize"
 
 document.addEventListener("turbo:load", () => {
   const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
   const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
 
+document.addEventListener("turbo:load", () => {
   autosize(document.querySelectorAll('textarea'))
 })
